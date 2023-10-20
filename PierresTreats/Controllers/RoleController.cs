9d2952bed0a97406180353490898d67f68cbdb4a
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-20 10:49:12 -0700

    Add RoleController

---
+++ PierresTreats/Controllers/RoleController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PierresTreats.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBook.Controllers
+{
+  public class RoleController : Controller  
+  {  
+    private readonly RoleManager<IdentityRole> _roleManager;  
+    private readonly UserManager<ApplicationUser> _userManager;  
+    public RoleController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)  
+    {  
+      _roleManager = roleManager;  
+      _userManager = userManager;  
+    }
+  }
+}
