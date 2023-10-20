52c8fd44f91b8afc552fc86b7e54b25059b6b1e7
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-20 09:56:33 -0700

    Add boilerplate code

---
+++ PierresTreats/Models/PierresTreatsContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RecipeBook.Models;
+
+namespace PierresTreats.Models
+{
+  public class PierresTreatsContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<Flavor> Flavors { get; set; }
+    public DbSet<TreatFlavor> TreatFlavors { get; set; }
+    public PierresTreatsContext(DbContextOptions options) : base(options) { }
+  }
+}
