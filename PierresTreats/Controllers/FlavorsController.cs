d0d695d8fd652871157b51c8238550bcd5d0064a
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-20 23:33:44 -0700

    Add GET and POST Create actions + corresponding View

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,10 +7,10 @@ using PierresTreats.Models;
 
 namespace PierresTreats.Controllers
 {
-  public class EngineersController : Controller
+  public class FlavorsController : Controller
   {
     private readonly PierresTreatsContext _db;
-    public EngineersController(PierresTreatsContext db)
+    public FlavorsController(PierresTreatsContext db)
     {
       _db = db;
     }
@@ -18,6 +19,28 @@ namespace PierresTreats.Controllers
     {
       List<Treat> allTreats = _db.Treats.ToList();
       return View(allTreats);
+    }
+
+     public ActionResult Create()
+    {
+      ViewBag.TreatId = new SelectList(_db.Treats, "TreatId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Flavor newFlavor)
+    {
+      if (!ModelState.IsValid)
+      {
+        ViewBag.MachineId = new SelectList(_db.Treats, "TreatId", "Name");
+        return View(newFlavor);
+      }
+      else
+      {
+        _db.Flavors.Add(newFlavor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
     }
   }
 }
