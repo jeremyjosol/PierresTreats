42a36f35a4bea0169e70a1c48f7de3d9a386e8fc
Jeremy Josol <jeremyjosol@gmail.com>
2023-10-21 10:18:42 -0700

    Add ErrorMessage validation

--- PierresTreats/Models/Flavor.cs
+++ PierresTreats/Models/Flavor.cs
@@ -6,6 +6,7 @@ namespace PierresTreats.Models
   public class Flavor 
   {
     public int FlavorId { get; set; }
+    [Required(ErrorMessage = "* Please enter a valid entry for this field.")]
     public string Type { get; set; }
     public List<TreatFlavor> JoinEntities { get; set; }
   }
--- PierresTreats/Models/Treat.cs
+++ PierresTreats/Models/Treat.cs
@@ -6,6 +6,7 @@ namespace PierresTreats.Models
   public class Treat
   {
     public int TreatId { get; set; }
+    [Required(ErrorMessage = "* Please enter a valid entry for this field.")]
     public string Name { get; set; }
     public List<TreatFlavor> JoinEntities { get; set; }
   }
